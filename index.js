8368d5295c45578249f39a15bb8c02b832ebdc09
Marcglacier32 <mark.thiongo@student.moringaschool.com>
2025-01-10 11:03:35 -0800

    Complete looping code-along

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+// Function to write thank-you cards
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  // Function to count down from a number to 0
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Sample usage of writeCards function
+  const thankYouMessages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  console.log(thankYouMessages);
+  
+  // Sample usage of countDown function
+  countDown(10);
+  
